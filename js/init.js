05c79a23f22e7406043ae8de523cff2442c06c26
N1bbler <riddledesignworks@gmail.com>
2016-03-08 22:42:42 -0800

    Updated case study theme and tweaked slow scroll JS to offset by header height

--- js/init.js
+++ js/init.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 	    var $target = $(target);
 
 	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
+	        'scrollTop': $target.offset().top - 85
 	    }, 900, 'swing');
 	});
     // slider
